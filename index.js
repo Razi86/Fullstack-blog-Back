52f04f91f630c9c67f70b97e8b0d86ae56e86018
Fernanda <fernanda.wynter@gmail.com>
2025-03-28 15:09:51 +0100

    home

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import client from './db.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
 const app= express();
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`);
-})
-
-app.get('/',async (req,res) => {
+app.get('/',async(req,res) => {
     try {
-         res.json({message: 'server is running'});
+        const result = await client.query('select * from posts');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+app.get('/posts/:id',async(req,res) => {
+    const {id} = req.params;
+    try {
+        const result = await client.query('select * from posts where id= $1',[id]);
+        res.json(result.rows);
     } catch (error) {
         res.status(500).send(error.message)
     }
 })
 
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`);
+})
